e227c80c463cc62f424701a24d501ae1138fc60d
notdryft <gui.corre@gmail.com>
2016-07-07 20:34:05 +0200

    Location switcher

---
+++ zshrc.d/locations.zsh
@@ -0,0 +1,18 @@
+
+function toggle-wifi() {
+  networksetup -setairportpower en0 "$1"
+}
+
+function switch-location() {
+  toggle-wifi "off"
+  scselect "$1"
+
+  case "$2" in
+    --with-wifi)
+      toggle-wifi "on"
+      ;;
+  esac
+}
+
+alias home='switch-location "Home" --with-wifi'
+alias auto='switch-location "Automatic"'
