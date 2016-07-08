6fe76812182e5f37dcf45512b41432ae7033b5ba
notdryft <gui.corre@gmail.com>
2016-07-08 15:59:54 +0200

    More aliases and tweaks

--- zshrc.d/vagrant.zsh
+++ zshrc.d/vagrant.zsh
@@ -2,7 +2,8 @@
 alias v=vagrant
 alias vd='vagrant destroy --force'
 alias vh='vagrant halt'
+alias vp='vagrant provision'
 alias vr='vagrant reload'
 alias vs="vagrant ssh $1"
 alias vu='vagrant up'
-alias vua='vagrant up --provider=aws'
+alias vua='bash -c "vagrant up --no-provision --parallel --provider=aws && vagrant provision"'
