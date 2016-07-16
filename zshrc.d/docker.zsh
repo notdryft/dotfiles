8a7f9e2b9f6876fc348fb97563dcf11a9dfdba27
notdryft <gui.corre@gmail.com>
2016-07-16 14:25:37 +0200

    Add dm create with insecure registry alias

--- zshrc.d/docker.zsh
+++ zshrc.d/docker.zsh
@@ -18,6 +18,13 @@ function docker-mrproper() {
   docker rmi -f $(docker images | sed 1d | awk '{ print $3 }')
 }
 
+function docker-machine-insecure-registry() {
+
+  set -x
+
+  docker-machine create --driver virtualbox --engine-insecure-registry $2 $1
+}
+
 alias dc=docker-compose
 alias de=docker-env
 alias di='docker images'
@@ -25,5 +32,6 @@ alias dpa='docker ps -a -q'
 alias dps='docker ps'
 alias dm=docker-machine
 alias dmc="dm create --driver virtualbox $1"
+alias dmci=docker-machine-insecure-registry
 alias dmp=docker-mrproper
 alias drm=docker-rm
