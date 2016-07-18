2889412027af66fa581a301accec2feea0fb33c0
notdryft <gui.corre@gmail.com>
2016-07-18 18:09:24 +0200

    Add cat + pbcopy alias

--- zshrc.d/tools.sh
+++ zshrc.d/tools.sh
@@ -1,3 +1,7 @@
+function cat-pbcopy() {
+  cat "$1" | pbcopy
+}
+
 function pwd-pbcopy() {
   pwd | pbcopy
 }
@@ -6,5 +10,6 @@ function touch-chmod() {
   touch "$1" && chmod u+x "$1"
 }
 
+alias catp=cat-pbcopy
 alias p=pwd-pbcopy
 alias tc=touch-chmod
