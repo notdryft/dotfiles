4b08ce5533e5eb38e02a54ab3858119c5531b230
notdryft <gui.corre@gmail.com>
2016-07-18 18:09:30 +0200

    Refine docker aliases

--- zshrc.d/docker.zsh
+++ zshrc.d/docker.zsh
@@ -14,8 +14,18 @@ function docker-rm() {
 
   set -x
 
+  for container in "$@"; do
+    docker stop "$container" && \
+      docker rm "$container"
+  done
+}
+
+function docker-rm-all() {
+
+  set -x
+
   docker stop $(docker ps -a -q) && \
-  docker rm $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
 }
 
 function docker-mrproper() {
@@ -27,11 +37,17 @@ function docker-mrproper() {
 
 alias dc=docker-compose
 alias de=docker-env
+alias dex='docker exec'
 alias di='docker images'
 alias dpa='docker ps -a -q'
 alias dps='docker ps'
+alias dl='docker logs'
 alias dm=docker-machine
+alias dn='docker network'
 alias dmc="dm create --driver virtualbox $1"
 alias dmci=docker-machine-insecure-registry
 alias dmp=docker-mrproper
+alias dr='docker run'
 alias drm=docker-rm
+alias drma=docker-rm-all
+alias drmi='docker rmi -f'
