30100ad82416f2b38f38bf8a0ffaf3be0e51fb22
notdryft <gui.corre@gmail.com>
2017-04-19 14:44:52 +0200

    Enhance brew update script

--- zshrc.d/brew.zsh
+++ zshrc.d/brew.zsh
@@ -1,6 +1,14 @@
-
 function buuc() {
   brew update && \
     brew upgrade && \
-    brew cleanup
+    brew cleanup -s && \
+    brew cask cleanup && \
+    brew doctor && \
+    brew missing && \
+    apm upgrade -c false
 }
+
+alias bsl='brew services list'
+
+export PATH=/usr/local/opt/gnu-getopt/bin:$PATH
+export PATH=/usr/local/opt/openssl/bin:$PATH
