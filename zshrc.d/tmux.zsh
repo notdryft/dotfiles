2c4a72e9c16313f050504e5e5f07b44630711edb
notdryft <gui.corre@gmail.com>
2017-04-19 14:44:18 +0200

    Add kill session script

--- zshrc.d/tmux.zsh
+++ zshrc.d/tmux.zsh
@@ -2,6 +2,16 @@ function tmux-attach() {
   tmux attach -t $1
 }
 
+function tmux-kill-session {
+  if [ "$#" -eq 0 ]; then
+    tmux kill-session -t $(tmux display-message -p '#S')
+  else
+    for session in $@; do
+      tmux kill-session -t $session
+    done
+  fi
+}
+
 function tmux-ls() {
   tmux ls
 }
@@ -11,5 +21,6 @@ function tmux-new() {
 }
 
 alias t=tmux-attach
+alias tk=tmux-kill-session
 alias tls=tmux-ls
 alias tn=tmux-new
