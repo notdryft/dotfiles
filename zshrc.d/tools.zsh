66814d1a19164256b5adc225b99fc4f3cc28c5b4
notdryft <gui.corre@gmail.com>
2017-04-19 14:42:15 +0200

    Add realpath shortcut

--- zshrc.d/tools.zsh
+++ zshrc.d/tools.zsh
@@ -6,10 +6,15 @@ function pwd-pbcopy() {
   pwd | pbcopy
 }
 
+function realpath-pbcopy() {
+  realpath "$1" | pbcopy
+}
+
 function touch-chmod() {
   touch "$1" && chmod u+x "$1"
 }
 
 alias catp=cat-pbcopy
 alias p=pwd-pbcopy
+alias realp=realpath-pbcopy
 alias tc=touch-chmod
