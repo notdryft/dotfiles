12a97ee2fa5dffba98c13250d7d93b50536206b5
notdryft <gui.corre@gmail.com>
2017-05-15 01:43:04 +0200

    Use NPM_CONFIG_PREFIX on Linux

--- zshrc.d/node.zsh
+++ zshrc.d/node.zsh
@@ -1,1 +1,9 @@
+case "`uname -s`" in
+  Linux)
+    export NPM_CONFIG_PREFIX=~/.npm-global
+    export PATH=${NPM_CONFIG_PREFIX}/bin:$PATH
+    ;;
+esac
+
+
 alias npmu='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f4); do npm -g install "$package"; done'
