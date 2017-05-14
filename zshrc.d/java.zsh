8632b40d32d01fc6fc7a037a780a03dac45815dc
notdryft <gui.corre@gmail.com>
2017-05-15 01:42:40 +0200

    Try openjdk on Linux

--- zshrc.d/java.zsh
+++ zshrc.d/java.zsh
@@ -1,6 +1,19 @@
-java_version="1.8.0_121"
+case "`uname -s`" in
+  Darwin)
+    java_version="1.8.0_121"
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk${java_version}.jdk/Contents/Home
-export JDK_HOME=/Library/Java/JavaVirtualMachines/jdk${java_version}.jdk/Contents/Home
+    export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk${java_version}.jdk/Contents/Home
+    export JDK_HOME=/Library/Java/JavaVirtualMachines/jdk${java_version}.jdk/Contents/Home
+
+    ;;
+  Linux)
+    java_short_version="1.8.0"
+    java_version="${java_short_version}.131-1.b12"
+
+    export JAVA_HOME=/usr/lib/jvm/java-${java_short_version}-openjdk-${java_version}.fc25.x86_64
+    export JDK_HOME=/usr/lib/jvm/java-${java_short_version}-openjdk-${java_version}.fc25.x86_64
+
+    ;;
+esac
 
 export PATH=${JAVA_HOME}/bin:$PATH
