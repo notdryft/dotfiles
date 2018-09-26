5af7bbdec5d143f2a3735b691d2e19498e8839d3
Guillaume Corré <gui.corre@gmail.com>
2018-09-26 09:39:30 +0200

    Updates to gcloud PATH and completions

--- zshrc.d/gcloud.zsh
+++ zshrc.d/gcloud.zsh
@@ -1,9 +1,9 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [[ -d /opt/google-cloud-sdk ]]; then
+if [ -f '/opt/google-cloud-sdk/path.zsh.inc' ]; then
   source '/opt/google-cloud-sdk/path.zsh.inc'
 fi
 
-# The next line enables zsh completion for gcloud.
-if [[ -d /opt/google-cloud-sdk ]]; then
+# The next line enables shell command completion for gcloud.
+if [ -f '/opt/google-cloud-sdk/completion.zsh.inc' ]; then
   source '/opt/google-cloud-sdk/completion.zsh.inc'
 fi
