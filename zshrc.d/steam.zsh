32e98c6b7769a4ce30c62352e79a29161790fb1a
Guillaume Corré <gui.corre@gmail.com>
2018-09-26 10:10:10 +0200

    Only alias steam on MacOS

--- zshrc.d/steam.zsh
+++ zshrc.d/steam.zsh
@@ -1,1 +1,6 @@
-alias steam='wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Steam/Steam.exe'
+case "`uname -s`" in
+  Darwin)
+    alias steam='wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Steam/Steam.exe'
+
+    ;;
+esac
