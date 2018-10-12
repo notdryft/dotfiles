cce1b4ca67b3449719db14125d64c1724432d613
Guillaume Corré <gui.corre@gmail.com>
2018-10-12 08:57:50 +0200

    Update OpenJDK

--- zshrc.d/java.zsh
+++ zshrc.d/java.zsh
@@ -8,10 +8,10 @@ case "`uname -s`" in
     ;;
   Linux)
     java_short_version="1.8.0"
-    java_version="${java_short_version}.131-1.b12"
+    java_version="${java_short_version}.181.b15-5"
 
-    export JAVA_HOME=/usr/lib/jvm/java-${java_short_version}-openjdk-${java_version}.fc25.x86_64
-    export JDK_HOME=/usr/lib/jvm/java-${java_short_version}-openjdk-${java_version}.fc25.x86_64
+    export JAVA_HOME=/usr/lib/jvm/java-${java_short_version}-openjdk-${java_version}.fc28.x86_64
+    export JDK_HOME=/usr/lib/jvm/java-${java_short_version}-openjdk-${java_version}.fc28.x86_64
 
     ;;
 esac
