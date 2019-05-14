e8df0adf0640d81c47276c9841f4749c60b7b1e7
Guillaume Corré <gui.corre@gmail.com>
2019-05-14 09:37:09 +0200

    Automated xinput search to enable natural scrolling, multiple monitor config + clean up

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,5 +4,12 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-polybar top &
-polybar bottom &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+    MONITOR=$m polybar --reload bottom &
+  done
+else
+  polybar --reload top &
+  polybar --reload bottom &
+fi
