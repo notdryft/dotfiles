619cc37365524f62a8d2b37890dae7ea746df27c
Guillaume Corré <gui.corre@gmail.com>
2019-05-14 09:17:56 +0200

    Use brew on MacOS only

--- zshrc.d/brew.zsh
+++ zshrc.d/brew.zsh
@@ -1,12 +1,18 @@
-function buuc() {
-  brew update && \
-    brew upgrade && \
-    brew cleanup -s && \
-    brew doctor && \
-    brew missing
-}
+case "`uname -s`" in
+  Darwin)
+    function buuc() {
+      brew update && \
+        brew upgrade && \
+        brew cleanup -s && \
+        brew doctor && \
+        brew missing
+    }
 
-alias bsl='brew services list'
+    export PATH=/usr/local/opt/gnu-getopt/bin:$PATH
+    export PATH=/usr/local/opt/openssl/bin:$PATH
 
-export PATH=/usr/local/opt/gnu-getopt/bin:$PATH
-export PATH=/usr/local/opt/openssl/bin:$PATH
+    alias bsl='brew services list'
+
+    ;;
+esac
+
