48fb768a58f1206004c2dc27d40adb1ac8ca50d1
Guillaume Corré <gui.corre@gmail.com>
2019-05-14 09:27:51 +0200

    Limit node configuration to MacOS

--- zshrc.d/node.zsh
+++ zshrc.d/node.zsh
@@ -1,13 +1,13 @@
 case "`uname -s`" in
-  Linux)
+  Darwin)
     export NPM_CONFIG_PREFIX=~/.npm-global
     export PATH=${NPM_CONFIG_PREFIX}/bin:$PATH
+
+    function npm-latest() {
+     npm i -g $1@$(npm show $1 versions --json | jq -r '.[-1]')
+    }
+
+    alias npmu='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f4); do npm -g install "$package"; done'
+    alias npml=npm-latest
     ;;
 esac
-
-function npm-latest() {
- npm i -g $1@$(npm show $1 versions --json | jq -r '.[-1]')
-}
-
-alias npmu='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f4); do npm -g install "$package"; done'
-alias npml=npm-latest
