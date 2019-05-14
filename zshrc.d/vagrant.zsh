1fc8a84fcdd67fa423596577defdfad81cc2bc3a
Guillaume Corré <gui.corre@gmail.com>
2019-05-14 09:30:10 +0200

    Add vagrant aliases

--- zshrc.d/vagrant.zsh
+++ zshrc.d/vagrant.zsh
@@ -4,6 +4,9 @@ alias vd='vagrant destroy --force'
 alias vh='vagrant halt'
 alias vp='vagrant provision'
 alias vr='vagrant reload'
+alias vra='vagrant rsync-auto'
 alias vs="vagrant ssh"
+alias vscp="vagrant scp"
 alias vu='vagrant up'
+alias vunop='vagrant up --no-provision'
 alias vua='bash -c "vagrant up --no-provision --parallel --provider=aws && vagrant provision"'
