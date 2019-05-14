5ae0346da16f86c7bb607afab533ca0e29b0c6d5
Guillaume Corré <gui.corre@gmail.com>
2019-05-14 09:30:32 +0200

    Add cargo aliases

--- zshrc.d/rust.zsh
+++ zshrc.d/rust.zsh
@@ -1,3 +1,13 @@
 if [ -f "$HOME/.cargo/env" ]; then
   source $HOME/.cargo/env
 fi
+
+alias cc='cargo check'
+alias ci='cargo install'
+alias cb='cargo build'
+alias cbd='cargo build --debug'
+alias cbr='cargo build --release'
+alias cdo='cargo doc --open'
+alias cr='cargo run'
+alias crv='cargo run --verbose'
+alias ct='cargo test'
