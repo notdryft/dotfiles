24884436772724b8b481d70ef3ac4340c4488f6a
Guillaume Corré <gui.corre@gmail.com>
2023-08-16 20:00:55 +0200

    Adapt brew zshrc.d script to Linux

--- zshrc.d/brew.zsh
+++ zshrc.d/brew.zsh
@@ -1,18 +1,21 @@
+function buuc() {
+  brew update && \
+    brew upgrade && \
+    brew cleanup -s && \
+    brew doctor && \
+    brew missing
+}
+
 case "`uname -s`" in
   Darwin)
-    function buuc() {
-      brew update && \
-        brew upgrade && \
-        brew cleanup -s && \
-        brew doctor && \
-        brew missing
-    }
-
-    export PATH=/usr/local/opt/gnu-getopt/bin:$PATH
-    export PATH=/usr/local/opt/openssl/bin:$PATH
+    #export PATH=/usr/local/opt/gnu-getopt/bin:$PATH
+    #export PATH=/usr/local/opt/openssl/bin:$PATH
 
     alias bsl='brew services list'
 
     ;;
-esac
+  Linux)
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
+    ;;
+esac
