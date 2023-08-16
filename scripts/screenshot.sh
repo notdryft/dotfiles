a63dab7e7e181fccecdac660d3353f336712cae1
Guillaume Corré <gui.corre@gmail.com>
2023-08-16 19:58:53 +0200

    Add screenshot script

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-timestamp=`date +%Y-%m-%d-%s`
+timestamp=$(date +%Y-%m-%d-%s)
 name=~/Pictures/${timestamp}_screenshot.png
 
-import $name
-convert -shave 1x1 -trim $name $name
+args=$(getopt -o s -l select -n "$0" -- "$@")
+eval set -- "$args"
+
+while true; do
+  case "$1" in
+    -s | --select ) select=true; shift ;;
+    * ) break ;;
+  esac
+done
+
+if [ "x$select" = "xtrue" ]; then
+  import "$name"
+else
+  import -window root "$name"
+fi
+
+convert -shave 1x1 -trim "$name" "$name"
