10587c223c748bc98d587d45a8d163158b03178f
Guillaume Corré <gui.corre@gmail.com>
2023-08-16 20:01:12 +0200

    Add docker aliases

--- zshrc.d/docker.zsh
+++ zshrc.d/docker.zsh
@@ -1,19 +1,14 @@
 function docker-env() {
-
   eval "$(docker-machine env $1)"
 }
 
 function docker-machine-insecure-registry() {
-
   set -x
-
   docker-machine create --driver virtualbox --engine-insecure-registry $2 $1
 }
 
 function docker-rm() {
-
   set -x
-
   for container in "$@"; do
     docker stop "$container" && \
       docker rm "$container"
@@ -21,17 +16,13 @@ function docker-rm() {
 }
 
 function docker-rm-all() {
-
   set -x
-
   docker stop $(docker ps -a -q) && \
     docker rm $(docker ps -a -q)
 }
 
 function docker-rm-none() {
-
   set -x
-
   docker images | \
     grep '<none>' | \
     awk '{ print $3 }' | \
@@ -39,27 +30,27 @@ function docker-rm-none() {
 }
 
 function docker-mrproper() {
-
   set -x
-
   docker rmi -f $(docker images -a -q)
 }
 
-alias dc=docker-compose
-alias de=docker-env
-alias dex='docker exec'
+alias dc='docker compose'
+alias de='docker exec'
+alias denv=docker-env
 alias di='docker images'
-alias dpa='docker ps -a -q'
-alias dps='docker ps'
+alias dh='docker history'
 alias dl='docker logs'
 alias dm=docker-machine
-alias dmc="dm create --driver virtualbox $1"
+alias dmc='dm create --driver virtualbox'
 alias dmci=docker-machine-insecure-registry
 alias dmp=docker-mrproper
 alias dn='docker network'
+alias dpa='docker ps -a -q'
+alias dps='docker ps'
 alias dr='docker run'
 alias drm=docker-rm
 alias drma=docker-rm-all
 alias drmi='docker rmi -f'
 alias drmn=docker-rm-none
+alias dsp='docker system prune'
 alias dv='docker volume'
