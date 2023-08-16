cf02974eb7d2e9056f816c9f91e5c0935871ba1a
Guillaume Corré <gui.corre@gmail.com>
2023-08-16 20:03:56 +0200

    Add version parameter to sbt aliases

--- zshrc.d/sbt.zsh
+++ zshrc.d/sbt.zsh
@@ -1,2 +1,15 @@
-alias sbuc='sbt update && sbt clean compile package publishM2'
-alias sbucc='sbt update && sbt ";clean;compile;package;^ publishLocal;^ publishM2"'
+function sbuc() {
+  set -x
+  if [ ! -z "$1" ]; then
+    params="-Dproject.version=$1"
+  fi
+  sbt $params 'clean;compile;package;publishLocal'
+}
+
+function sbucc() {
+  set -x
+  if [ ! -z "$1" ]; then
+    params="-Dproject.version=$1"
+  fi
+  sbt $params 'clean;compile;package;^ publishLocal;^ publishM2'
+}
