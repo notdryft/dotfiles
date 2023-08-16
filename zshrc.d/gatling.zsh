cd97b16a106730794313231dffc946d932b9f8bc
Guillaume Corré <gui.corre@gmail.com>
2023-08-16 20:07:39 +0200

    Update tmux gatling layout

--- zshrc.d/gatling.zsh
+++ zshrc.d/gatling.zsh
@@ -4,7 +4,7 @@ function ec2() {
 
 function tmux-gatling() {
   session_name=gatling
-  root_dir=~/Code/gatling
+  root_dir=~/Code/projects/gatling
 
   # http://sherifsoliman.com/2016/12/29/tmux-workspace-scripts/
 
@@ -12,21 +12,53 @@ function tmux-gatling() {
 
   tmux new -d \
     -s ${session_name} \
-    -c ${root_dir}/core/gatling \
+    -c ${root_dir}/core/gatling-private \
     -n gatling
-  tmux rename-window gatling
+
   tmux split-window -h
-  tmux send-keys "cd ${root_dir}/core/gatling-highcharts-private && clear" C-m
+  tmux send-keys "cd ${root_dir}/core/gatling && clear" C-m
   tmux select-pane -t 0
 
-  # FrontLine window
+  # Cassandra window
 
-  frontline_dir=${root_dir}/pro/frontline
-  dashboard_dir=${frontline_dir}/frontline-dashboard
+  cloud_dir=${root_dir}/core/frontline-cloud
+  self_hosted_dir=${root_dir}/core/frontline
 
   tmux new-window \
-    -c ${frontline_dir} \
-    -n frontline
+    -c ${self_hosted_dir} \
+    -n cassandra
+
+  tmux split-window -h
+  tmux send-keys "cd ${cloud_dir} && clear" C-m
+  tmux select-pane -t 0
+
+  # Cloud window
+
+  front_dir=${cloud_dir}/front
+  dashboard_dir=${front_dir}/dashboard
+
+  tmux new-window \
+    -c ${cloud_dir}/back \
+    -n cloud
+
+  ## Dashboard pane
+
+  tmux split-window -h
+  tmux send-keys "cd ${dashboard_dir} && clear" C-m
+
+  ## Front pane
+
+  tmux select-pane -t 1
+  tmux split-window
+  tmux send-keys "cd ${front_dir} && clear" C-m
+
+  # Self-Hosted window
+
+  dashboard_dir=${self_hosted_dir}/frontline-dashboard
+
+  tmux new-window \
+    -c ${self_hosted_dir} \
+    -n self-hosted
 
   ## Dashboard pane
 
@@ -39,29 +71,9 @@ function tmux-gatling() {
   tmux split-window
   tmux send-keys "cd ${dashboard_dir} && clear" C-m
 
-  # Installer window
+  # Go back to FrontLine Cloud pane and attach
 
-  automation_dir=${root_dir}/automation
-
-  tmux new-window \
-    -c ${automation_dir}/installer \
-    -n installer
-  tmux split-window -h
-  tmux send-keys "cd ${automation_dir}/installer && clear" C-m
-  tmux select-pane -t 0
-
-  # Containers window
-
-  tmux new-window \
-    -c ${automation_dir}/containers \
-    -n containers
-  tmux split-window -h
-  tmux send-keys "cd ${automation_dir}/containers && clear" C-m
-  tmux select-pane -t 0
-
-  # Go back to Frontline:API pane and attach
-
-  tmux select-window -t frontline
+  tmux select-window -t cloud
   tmux select-pane -t 0
   tmux attach-session -t ${session_name}
 }
